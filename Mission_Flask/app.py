2bfbbceb058f37a8294d8817b275e9198943118b
Abishua Prashanth <abishua73@gmail.com>
2021-12-12 12:31:28 -0500

    Added code to insert the data into Mongo DB

--- Mission_Flask/app.py
+++ Mission_Flask/app.py
@@ -1,28 +1,31 @@
-from flask import Flask, render_template
-import pymongo
-#import insert_data
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
 import scrape_mars
 
 #Initialize flask app
 app = Flask(__name__)
 
 #set up Mongo connection
-conn = "mongodb://localhost:27017"
-client = pymongo.MongoClient(conn)
+mongo  = PyMongo(app, uri="mongodb://localhost:27017/Mars_DB")
 
 @app.route("/")
 def index():
+    mars_data = mongo.db.collection.find_one()
     #Display the home page
-    return render_template("index.html")
+    print(f"======== {mars_data}")
+    return render_template("index.html", data = mars_data)
 
 @app.route("/scrape")
 def scrape():
-
     mars_data = scrape_mars.scrape_info()
     data = scrape_mars.scrape_info()
     print(data)
-    #Display the home page
-    return render_template("index.html")
+
+    # Insert into the Mongo database     
+    mongo.db.collection.insert_one(data)
+    # Redirect back to the home page
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
